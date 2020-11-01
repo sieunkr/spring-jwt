ba73a4f4637e92b63d249190cb875219e09192eb
sieunkim <sieunkr@gmail.com>
2020-11-01 13:12:21 +0900

    jssionid

---
+++ spring-default/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/coffees")
+public class HomeController {
+
+    @GetMapping
+    public String getCoffee(HttpSession session) {
+
+        log.info(session.getId());
+
+        return "coffee";
+    }
+}
